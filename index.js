d240d4c15b9c71cc9c3596964cdb95b72dff70bf
Diptiranjan Sukla <sukla.dipti@gmail.com>
2020-08-06 15:28:29 +0530

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let x = 10;
 const express = require("express");
-
+console.log("git merge simple example with clean understanding");
 //just to dummy variable and import to demonstrate git merge
+//let me create different git repo and push
